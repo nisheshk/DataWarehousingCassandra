a1ee77b73176167e6e9a7a280e682cad031dc219
Nishesh Kalakheti <nissessk@gmail.com>
2020-08-04 16:40:35 -0400

    Adds logs to the file and makes script more dynamic.

--- spark_scripts/cassandra_connect_pyspark.py
+++ spark_scripts/cassandra_connect_pyspark.py
@@ -1,27 +1,57 @@
 from    pyspark.sql import SparkSession
-from pyspark.sql.types import *
-import pyspark.sql.functions as F
-from pyspark.sql.functions import col, to_timestamp
+from    pyspark.sql.types import *
+import  pyspark.sql.functions as F
+from    pyspark.sql.functions import col, to_timestamp
+import  argparse
+import  sys
+from    logger import logging
+from    ddl_script import create_user_info_table
 
-
-def write_to_cassandra(df):
-    df.write.format("org.apache.spark.sql.cassandra").mode('append').options(table="user_info", keyspace="testing")\
+def write_to_cassandra(df, table, keyspace):
+    df.write.format("org.apache.spark.sql.cassandra").mode('append').options(table=table, keyspace=keyspace)\
         .option("inferSchema",'true')\
-        .option("spark.cassandra.connection.host", "172.17.0.2")\
-        .option("spark.cassandra.connection.host", "172.17.0.3")\
-        .option("spark.cassandra.connection.port", "9042").save()
+        .option("spark.cassandra.connection.host","172.17.0.2")\
+        .option("spark.cassandra.connection.host","172.17.0.3")\
+        .save()
 
 def read_csv_file(path):
     
-    df=spark.read.option("delimiter", ",").option('header','true').csv(path)
+    df = spark.read.option("delimiter", ",").option('header','true').csv(path)
     df = df.withColumn('event_time', to_timestamp(df['event_time'], format='yyyy-MM-dd HH:mm:ss z'))
-    
+    df = df.withColumn('year',F.year(df.event_time))
+    df = df.withColumn("week", F.date_format(F.col("event_time"), "w"))
     return df
 
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("load-data-into_cassandra").getOrCreate()
-    csv_path = "raw_data.csv"
-    df = read_csv_file(csv_path)
-    write_to_cassandra(df)
-    
+    try:    
+        logger = logging.getLogger()
+        fhandler = logging.FileHandler(filename='data_load_to_cassandra.log', mode='w')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fhandler.setFormatter(formatter)
+        logger.addHandler(fhandler)
+        logger.setLevel(logging.INFO)
+        
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--cass_keyspace", help="keyspace")
+        parser.add_argument("--cass_table", help="table")
+        parser.add_argument("--incremental_run", help="Full table load or incremental run")
+        parser.add_argument("--csv_file", help="input file")
+
+        args = parser.parse_args()
+        if not (args.cass_keyspace and args.cass_table and args.incremental_run and args.csv_file):
+            logging.error("Command line arguments are missing. Possibly --cass_keyspace --cass_table --csv_file --incremental_run ")
+            sys.exit()
+        if args.incremental_run not in ['0','1']:
+            logging.error("Incremental run should be either 0 or 1")
+            sys.exit()
+        
+        spark = SparkSession.builder.appName("load-data-into_cassandra").getOrCreate()
+        incremental_run = int(args.incremental_run)
+        create_user_info_table(incremental_run, args.cass_keyspace, args.cass_table, logger)
+        df = read_csv_file(args.csv_file)
+        write_to_cassandra(df, args.cass_table, args.cass_keyspace)
+        
+    except Exception as e:
+        logging.error('{0}'.format(e))
+        sys.exit()
--- spark_scripts/transformation_2.py
+++ spark_scripts/transformation_2.py
@@ -1,53 +1,123 @@
 from    pyspark.sql import SparkSession
-from pyspark.sql import functions as F
-from pyspark.sql.types import StringType
-import pyspark
-import os
+from    pyspark.sql import functions as F
+from    pyspark.sql.types import StringType
+import  pyspark
+import  os
+import  sys
+from    logger import logging
+import  argparse
+
 
 def read_from_cassandra():
-    df=spark.read.format("org.apache.spark.sql.cassandra").option("spark.cassandra.connection.host", "172.17.0.2")\
-              .option("spark.cassandra.connection.host", "172.17.0.3")\
-              .option("spark.cassandra.connection.port", "9042").option("keyspace", "testing")\
-              .option("table", "user_info")\
-              .load()
-    return df
+    try:
+        logging.info('Read from_cassandra in progress')
+        column_names = ["event_time","user_session"]
+        if incremental_run:
+            today_date = datetime.date.today()
+            year, week_num, day_of_week = today_date.isocalendar()
+            incremental_condition = (F.col("year") == year) & (F.col("week") == week_num)
+            
+            df=spark.read.format("org.apache.spark.sql.cassandra")\
+                      .option("spark.cassandra.connection.port", "9042").option("keyspace", keyspace)\
+                      .option("table", table)\
+                      .load()\
+                      .select(column_names)\
+                      .where(incremental_condition)
+            df = df.filter(day(df.event_time) == today_date.day)
+        else:
+            df=spark.read.format("org.apache.spark.sql.cassandra")\
+                      .option("spark.cassandra.connection.port", "9042").option("keyspace", keyspace)\
+                      .option("table", table)\
+                      .load()\
+                      .select(column_names)
+
+        logging.info('Dataframe loaded successfully')
+        return df
+    
+    except Exception as e:
+        logging.error('Error in read_from_cassandra() function: {0}'.format(e))
+        raise e
 
 def get_user_count_by_hour(df):
-    df.createOrReplaceTempView('df')
-    result = \
-        spark.sql('''
+    try:
+        logging.info('Getting user count by hour in progress')
+        df.createOrReplaceTempView('df')
+        result = \
+            spark.sql('''
 
-        with cte1 AS (
-        SELECT DATE(event_time) as date, HOUR(event_time) as hour,
-            1 as count
-        FROM df
-            GROUP BY
-        user_session,DATE(event_time), HOUR(event_time)
-        )
-        SELECT to_timestamp(CONCAT(cast(date as string),"/",cast(hour as string),":00:00"), "yyyy-MM-dd/HH:mm:ss") as timestamp,
-        date, hour, SUM(count) as count FROM cte1 GROUP BY date,hour
-    ''')
-##    result = result.withColumn("day",result["day"].cast(StringType()))
-##    result = result.groupBy("year","month").agg(
-##        F.map_from_entries(\
-##        F.collect_list(\
-##        F.struct("day", "count"))).alias("user_count"))
-##    return result
-    return result
+            with cte1 AS (
+            SELECT DATE(event_time) as date1, HOUR(event_time) as hour,
+                1 as count
+            FROM df
+                GROUP BY
+            user_session,DATE(event_time), HOUR(event_time)
+            )
+            SELECT to_timestamp(CONCAT(cast(date1 as string),"/",cast(hour as string),":00:00"), "yyyy-MM-dd/HH:mm:ss") as date,
+            YEAR(date1) as year, MONTH(date1) as month, DAY(date1) as day, hour, SUM(count) as count FROM cte1 GROUP BY date1,hour
+        ''')
+    ##    result = result.withColumn("day",result["day"].cast(StringType()))
+    ##    result = result.groupBy("year","month").agg(
+    ##        F.map_from_entries(\
+    ##        F.collect_list(\
+    ##        F.struct("day", "count"))).alias("user_count"))
+    ##    return result
+        logging.info('Got User count by hour successfully')
+        return result
+    
+    except Exception as e:
+        logging.error('Error in get_user_count_by_hour() function: {0}'.format(e))
+        raise e 
 
-def write_to_mongo(df):
-    df.write.format("mongo").mode("overwrite").option("database","database_project").option("collection", "user_count_by_hour").save()
+def write_to_mongo(df, database, collection, incremental_run):
+    try:
+        logging.info('Write to MongoDB in progress')
+        write_mode = "overwrite"
+        if incremental_run:
+            write_mode = "append"
+        df.write.format("mongo").mode(write_mode).option("database",database).option("collection", collection).save()
+        logging.info('Write to MongoDB completed successfully')
+
+    except Exception as e:
+        logging.error('Error in write_to_mongo() function: {0}'.format(e))
+        raise e
 
 
 if __name__ == "__main__":
+    try:    
+        logger = logging.getLogger()
+        fhandler = logging.FileHandler(filename='user_count_by_day.log', mode='w')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fhandler.setFormatter(formatter)
+        logger.addHandler(fhandler)
+        logger.setLevel(logging.INFO)
+        
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--cass_keyspace", help="keyspace")
+        parser.add_argument("--cass_table", help="table")
+        parser.add_argument("--mongo_db", help="Mongo db")
+        parser.add_argument("--mongo_collection", help="Mongo collection")
+        parser.add_argument("--incremental_run", help="Full table load or incremental run")
+        
+        args = parser.parse_args()
+        if not (args.cass_keyspace and args.cass_table and args.mongo_db and args.mongo_collection and args.incremental_run):
+            logging.error("Command line arguments are missing. Possibly --cass_keyspace --cass_table --mongo_db --mongo_collection --incremental_run ")
+            sys.exit()
+        if args.incremental_run not in ['0','1']:
+            logging.error("Incremental run should be either 0 or 1")
+            sys.exit()
+        incremental_run = int(args.incremental_run)
 
-    spark = pyspark.sql.SparkSession.builder\
-    .appName('test-mongo')\
-    .master('local[*]')\
-    .config("spark.mongodb.input.uri", "mongodb://127.0.0.1:27017/test.coll") \
-    .config("spark.mongodb.output.uri", "mongodb://127.0.0.1:27017/test.coll") \
-    .getOrCreate()
-    df = read_from_cassandra()
-    total_user_per_day_df = get_user_count_by_hour(df)
-    #df = spark.createDataFrame([("Bilbo Baggins",  50), ("Gandalf", 1000), ("Gandalf", 195), ("Gandalf", 178), ("Kili", 77),("Kili", 169), ("Oin", 167), ("Gloin", 158)])
-    write_to_mongo(total_user_per_day_df)
+        logging.info("Argument parsed successfully")
+        spark = pyspark.sql.SparkSession.builder\
+                    .appName('test-mongo')\
+                    .master('local[*]')\
+                    .config("spark.mongodb.input.uri", "mongodb://192.168.2.80:30002") \
+                    .config("spark.mongodb.output.uri", "mongodb://192.168.2.80:30002") \
+                    .getOrCreate()
+        df = read_from_cassandra()
+        total_user_per_hour_df = get_user_count_by_hour(df)
+        write_to_mongo(total_user_per_hour_df)
+
+    except Exception as e:
+        logging.error('{0}'.format(e))
+        sys.exit()
